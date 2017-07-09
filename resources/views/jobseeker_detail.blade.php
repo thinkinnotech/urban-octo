b367dd6475057f3825dc9bc4272041aa4bce83d0
Manish <manish011994@gmail.com>
2017-07-10 01:35:13 +0530

    Job Seeker Page and Detail Page Completed.

---
+++ resources/views/jobseeker_detail.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Manish
+ * Date: 10/07/2017
+ * Time: 12:34 AM
+ */
---
+++ resources/views/jobseeker_search.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Manish
+ * Date: 09/07/2017
+ * Time: 1:05 AM
+ */
